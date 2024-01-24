fb3dbdce6885643a2320505054c9e6a7f00032d7
ariana gutierrez <carla_ari.97@live.com>
2024-01-24 01:35:43 +0100

    update

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-window.addEventListener("load", showModal); // Mostra il modal quando la pagina viene caricata
-
 const displayPlayer = document.querySelector(".displayPlayer");
 const gameBoard = document.querySelector(".gameBoard");
-const restartButton = document.querySelector(".restart");
+const restart = document.querySelector(".restart");
 
 let currentPlayer = "Player"; // Inizia con il Player
 let gameActive = false; // Inizia il gioco solo dopo la scelta del segno
@@ -11,6 +9,29 @@ let cells = [];
 
 let userSign = ""; // Memorizza il segno scelto dall'utente
 let computerSign = ""; // Memorizza il segno del computer
+
+window.addEventListener("load", showModal); // Mostra il modal quando la pagina viene caricata
+
+// Funzione per mostrare il modal
+function showModal() {
+    const modal = document.getElementById("pickSignModal");
+    modal.style.display = "flex";
+}
+
+// Inizia il gioco solo dopo che il segno è stato scelto
+function startGameWithSign(sign) {
+    userSign = sign;
+    computerSign = userSign === "X" ? "O" : "X";
+
+    const modal = document.getElementById("pickSignModal");
+    modal.style.display = "none";
+
+    currentPlayer = "Player";
+    gameActive = true; // Abilita il gioco
+    createGameBoard();
+}
+document.getElementById("chooseX").addEventListener("click", () => startGameWithSign("X"));
+document.getElementById("chooseO").addEventListener("click", () => startGameWithSign("O"));
 
 // Crea le celle del gioco
 function createGameBoard() {
@@ -35,14 +56,13 @@ function handleCellClick(cell) {
     if (checkWin(sign)) {
         displayPlayer.textContent = currentPlayer + " wins!";
         gameActive = false;
-        restartButton.style.display = "block";
+        restart.style.display = "block";
     } else if (moves === 9) {
         displayPlayer.textContent = "It's a tie!";
         gameActive = false;
-        restartButton.style.display = "block";
+        restart.style.display = "block";
     } else {
         currentPlayer = currentPlayer === "Player" ? "Computer" : "Player";
-        displayPlayer.style.display = "block";
         displayPlayer.textContent =  `${currentPlayer}'s turn`;
 
         if (currentPlayer === "Computer") {
@@ -51,7 +71,7 @@ function handleCellClick(cell) {
     }
 }
 
-// Funzione per controllare se c'è una vittoria
+// Funzione per determinare il vincitore
 function checkWin(sign) {
     const winPatterns = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -73,53 +93,19 @@ function makeComputerMove() {
             const randomIndex = Math.floor(Math.random() * emptyCells.length);
             const randomCell = emptyCells[randomIndex];
             handleCellClick(randomCell);
-    }, 1400); // Aspetta 1.2 secondi prima di far muovere il computer
+    }, 1500);
 }
-
-
 
 // Funzione per riavviare il gioco
 function restartGame() {
     showModal();
     
     cells.forEach((cell) => {
-    cell.textContent = "";
+        cell.textContent = "";
     });
 
     currentPlayer = "Player";
-    displayPlayer.style.display = "none";
     gameActive = false;
     moves = 0;
-    restartButton.style.display = "none";
 }
-
-restartButton.addEventListener("click", restartGame);
-createGameBoard();
-
-
-// Funzione per mostrare il modal
-function showModal() {
-    const modal = document.getElementById("pickSignModal");
-    const overlay = document.getElementById("overlay");
-    modal.style.display = "block";
-    overlay.style.display = "block";
-
-    restartButton.style.display = "none";
-}
-
-// Inizia il gioco solo dopo che il segno è stato scelto
-function startGameWithSign(sign) {
-    userSign = sign;
-    computerSign = userSign === "X" ? "O" : "X";
-
-    const modal = document.getElementById("pickSignModal");
-    const overlay = document.getElementById("overlay");
-    modal.style.display = "none";
-    overlay.style.display = "none";
-
-    currentPlayer = "Player";
-    gameActive = true; // Abilita il gioco
-}
-
-document.getElementById("chooseX").addEventListener("click", () => startGameWithSign("X"));
-document.getElementById("chooseO").addEventListener("click", () => startGameWithSign("O"));
+// restartButton.addEventListener("click", restartGame);
